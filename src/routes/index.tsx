1c529f5f554ed3e33197f209011845f9f18da290
zubair <zubair.apps22@gmail.com>
2026-09-15 20:24:04 -0700

    route change

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router"
 import DashboardPage from "@/pages/dashboard"
-import AppointmentsPage from "@/pages/appointments"
+// import AppointmentsPage from "@/pages/appointments"
 
 export const Route = createFileRoute("/")({
   component: DashboardPage,
